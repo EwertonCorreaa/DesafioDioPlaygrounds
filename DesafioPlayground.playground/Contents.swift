32527f4e8bdce6f099846d08c37ae098ef34ac80
Ewerton Correa <enascimentooo60@gmail.com>
2023-09-04 12:12:37 -0300

    Primeiro Commit

---
+++ DesafioPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+let name: String = "Steve"
+var lastName: String? = "Jobs"
+let unwrappedLastName: String = lastName ?? "Wozniak"
+
+print("\(name) \(unwrappedLastName)")
+
+if let lastName {
+    print("\(name) \(lastName)")
+} else {
+    print("\(name) Wozniak")
+}
